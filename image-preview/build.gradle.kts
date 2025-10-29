c5e686a52b3584d910ccaa2d9dac0117ff034fb7
WahajMubeen <iwahajmubeen341@gmail.com>
2025-10-29 15:36:20 +0500

    Initial commit

---
+++ image-preview/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.example.image_preview"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+        disable += listOf("NullSafeMutableLiveData")
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.foundation.android)
+    implementation(libs.foundation.layout.android)
+    compileOnly(libs.androidx.material3.android)
+    compileOnly(libs.androidx.ui.tooling.preview.android)
+    compileOnly(libs.coil.compose)
+    // ✅ Add these for testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.WahajMubeen341"
+                artifactId = "network-monitor-compose"
+                version = "1.0.0"
+            }
+        }
+    }
+}
+
+
